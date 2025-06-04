0099ab1ba369b7936cee189ddb35240683256443
Aziz Rayene Delaa <adelaa@dti.crosemont.quebec>
2025-06-03 20:38:11 -0400

    deploiment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/glitch-anime-azizz/',
+  base: mode === 'production' ? '/glitch-anime-azizz/' : '/',
   server: {
     host: "::",
     port: 8080,
